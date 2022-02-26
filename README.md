7e40ae3da55c5ec3b9110ce6ca0b42a9a6d812ae
Aryan Gupta <arynn.gupta@gmail.com>
2022-02-26 19:01:33 +0530

    added README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# About
+
+This is an API for the Harmony OS Neumorphism Library.
+
+# Endpoints
+
+https://neumorphism-api.herokuapp.com/neumorphism/living_room
+https://neumorphism-api.herokuapp.com/neumorphism/smart_watch
--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 let port = process.env.PORT || 3000;
 
-const smart_watch = require('./smart_watch.json');
 const living_room = require('./living_room.json');
+const smart_watch = require('./smart_watch.json');
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.get('/neumorphism/smart_watch', (req, res) => {
-  res.send(smart_watch);
+  res.send('This is an API for the Harmony OS Neumorphism Library.');
 });
 
 app.get('/neumorphism/living_room', (req, res) => {
   res.send(living_room);
+});
+
+app.get('/neumorphism/smart_watch', (req, res) => {
+  res.send(smart_watch);
 });
 
 app.listen(port, () => {
